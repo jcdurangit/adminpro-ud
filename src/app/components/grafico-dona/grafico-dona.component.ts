2c98bb92e9ec2066e5f6e4ddc3debccc833dc1bb
Juan Carlos Duran <jc_x9@hotmail.com>
2018-10-25 08:37:19 -0400

    Input, Output y viewchild

---
+++ src/app/components/grafico-dona/grafico-dona.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-grafico-dona',
+  templateUrl: './grafico-dona.component.html',
+  styleUrls: []
+})
+export class GraficoDonaComponent implements OnInit {
+
+  // Doughnut
+  @Input() doughnutChartLabels: string[] = [];
+  @Input() doughnutChartData: number[] = [];
+  @Input() doughnutChartType: string = '';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,7 +5,16 @@ import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { PAGES_ROUTES } from './pages.routes';
+import { FormsModule } from '@angular/forms';
 
+//NG2-CHAR
+
+import { ChartsModule } from 'ng2-charts';
+
+
+// temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 
 @NgModule({
@@ -13,7 +22,9 @@ import { PAGES_ROUTES } from './pages.routes';
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
-        Graficas1Component
+        Graficas1Component,
+        IncrementadorComponent,
+        GraficoDonaComponent
     ],
 
     exports: [
@@ -24,7 +35,9 @@ import { PAGES_ROUTES } from './pages.routes';
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
     ]
 })
 
